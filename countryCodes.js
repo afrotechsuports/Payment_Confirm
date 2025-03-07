39f7e059a03a48eeff142dd9ddaea0c7140ecbc6
Afro Tech Support <114155276+afrotechsuports@users.noreply.github.com>
2025-03-07 15:37:23 +0300

    asd

---
+++ countryCodes.js
@@ -0,0 +1,8 @@
+const countryCodes = [
+    { name: "Ethiopia", code: "+251" },
+    { name: "United States", code: "+1" },
+    { name: "United Kingdom", code: "+44" },
+    { name: "Kenya", code: "+254" },
+    { name: "Nigeria", code: "+234" },
+    // Add more countries as needed
+];
